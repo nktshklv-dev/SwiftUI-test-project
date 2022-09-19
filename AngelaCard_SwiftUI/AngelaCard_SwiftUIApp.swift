bbe5083374e4e370bcdd69d31f4875aaf526a734
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-09-19 18:07:13 +0300

    Initial Commit

---
+++ AngelaCard_SwiftUI/AngelaCard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngelaCard_SwiftUIApp.swift
+//  AngelaCard_SwiftUI
+//
+//  Created by Nikita  on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AngelaCard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
